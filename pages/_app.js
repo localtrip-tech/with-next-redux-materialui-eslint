e5ee4f21f39e16422d8b8b18b1084c221a40b3de
Shashank Shekhar <shashank7200@gmail.com>
2018-10-30 16:19:52 +0530

    update: jss in lifecycle, theme moved to upper level wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,58 +1,55 @@
 import App, { Container } from "next/app";
-import React from "react";
-import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
-import blue from "@material-ui/core/colors/blue";
+import React, { Fragment } from "react";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 import withReduxStore from "../lib/with-redux-store";
 import { Provider } from "react-redux";
-import indigo from "@material-ui/core/colors/indigo";
-import pink from "@material-ui/core/colors/pink";
-import red from "@material-ui/core/colors/red";
-
-const theme = createMuiTheme({
-  palette: {
-    primary: indigo,
-    secondary: pink,
-    error: red,
-    // Used by `getContrastText()` to maximize the contrast between the background and
-    // the text.
-    contrastThreshold: 3,
-    // Used to shift a color's luminance by approximately
-    // two indexes within its tonal palette.
-    // E.g., shift from Red 500 to Red 300 or Red 700.
-    tonalOffset: 0.2,
-    type: "dark"
-  },
-  spacing:{
-    unit: 12
-  },
-  typography: {
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"'
-    ].join(","),
-    useNextVariants: true,
-    fontSize: 16
-  }
-});
+import Head from "next/head";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import JssProvider from "react-jss/lib/JssProvider";
+import getPageContext from "./../lib/get-page-context";
 
 class MyApp extends App {
+  constructor(props) {
+    super(props);
+    this.pageContext = getPageContext();
+  }
+
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
   render() {
     const { Component, pageProps, reduxStore } = this.props;
     return (
       <Container>
         <Provider store={reduxStore}>
-          <MuiThemeProvider theme={theme}>
-            <Component {...pageProps} />
-          </MuiThemeProvider>
+          <Fragment>
+            <Head>
+              <title>My page</title>
+            </Head>
+            {/* Wrap every page in Jss and Theme providers */}
+            <JssProvider
+              registry={this.pageContext.sheetsRegistry}
+              generateClassName={this.pageContext.generateClassName}
+            >
+              {/* MuiThemeProvider makes the theme available down the React
+              tree thanks to React context. */}
+              <MuiThemeProvider
+                theme={this.pageContext.theme}
+                sheetsManager={this.pageContext.sheetsManager}
+              >
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                {/* Pass pageContext to the _document though the renderPage enhancer
+                to render collected styles on server side. */}
+                <Component pageContext={this.pageContext} {...pageProps} />
+              </MuiThemeProvider>
+            </JssProvider>
+          </Fragment>
         </Provider>
       </Container>
     );
